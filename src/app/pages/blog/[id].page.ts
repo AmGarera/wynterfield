16d477527b7a3e059fd7f01ea0d662e9f7b48088
Anthony Garera <gareraanthony@gmail.com>
2024-07-21 23:19:18 -0400

    Updating by adding title to post

--- src/app/pages/blog/[id].page.ts
+++ src/app/pages/blog/[id].page.ts
@@ -13,6 +13,7 @@ import { ActivatedRoute } from '@angular/router';
   template: `
     @if (post$ | async; as post) {
     <article class="max-w-4xl mx-auto p-5">
+      <h1 class="text-4xl font-bold mb-8">{{ post.items[0].title }}</h1>
       <img class="post__image w-full h-auto object-cover object-center mb-8" [src]="post.items[0].image" />
       <div class="prose prose-lg max-w-none" [innerHTML]="post.items[0].content_html"></div>
     </article>
